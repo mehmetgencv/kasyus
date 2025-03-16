2eb043cffaf1513a539aa47011d646029de1b099
Mehmet Genc <mehmetgencv@gmail.com>
2025-03-16 10:03:15 +0300

    add cart-service

--- docker-compose/prod/init-scripts/init-multiple-dbs.sql
+++ docker-compose/prod/init-scripts/init-multiple-dbs.sql
@@ -2,3 +2,10 @@ CREATE DATABASE kasyus_products;
 CREATE DATABASE kasyus_orders;
 CREATE DATABASE kasyus_users;
 CREATE DATABASE kasyus_auth;
+CREATE DATABASE kasyus_carts;
+CREATE DATABASE kasyus_inventory;
+CREATE DATABASE kasyus_reviews;
+CREATE DATABASE kasyus_shipping;
+CREATE DATABASE kasyus_payments;
+CREATE DATABASE kasyus_discounts;
+
